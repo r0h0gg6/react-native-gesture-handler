0e583f3a2bf546a11c338c1a91a3c6cb06d80987
r0h0gg6 <richard.heng0@walmart.com>
2025-07-28 04:02:43 -0700

    Add reanimated configs

--- android/src/main/java/com/swmansion/gesturehandler/ReanimatedEventDispatcher.kt
+++ android/src/main/java/com/swmansion/gesturehandler/ReanimatedEventDispatcher.kt
@@ -2,11 +2,16 @@ package com.swmansion.gesturehandler
 
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.uimanager.events.Event
+import com.swmansion.reanimated.ReanimatedModule
 
 class ReanimatedEventDispatcher {
-  @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
-  // This is necessary on new architecture
+  private var reanimatedModule: ReanimatedModule? = null
+
   fun <T : Event<T>>sendEvent(event: T, reactApplicationContext: ReactContext) {
-    // no-op
+    if (reanimatedModule == null) {
+      reanimatedModule = reactApplicationContext.getNativeModule(ReanimatedModule::class.java)
+    }
+
+    reanimatedModule?.nodesManager?.onEventDispatch(event)
   }
 }
